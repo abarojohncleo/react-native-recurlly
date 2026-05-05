7f9c0fe481f247cc7188699a3f013059f15f9e44
johnca-utanga <johnca@utanga.tech>
2026-05-06 00:05:55 +0800

    adding new home tab

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,5 @@
-import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Goto onboarding
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Sign Up
-      </Link>
-      <Link href="/subscriptions/spotify">Spotify Subscriptions</Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-      >
-        Claude Max Subscriptions
-      </Link>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="./(tabs)" />;
 }
