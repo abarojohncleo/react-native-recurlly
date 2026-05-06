cb1a502347c77e962d9076b2bd6de3cece70218b
johnca-utanga <johnca@utanga.tech>
2026-05-07 04:02:05 +0800

    feature: project set up, native wind, routing, tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,64 @@
+import { tabs } from "@/assets/constants/data";
+import { colors, components } from "@/assets/constants/theme";
+import clsx from "clsx";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const TabLayout = () => (
-  <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen name="index" options={{ title: "Home" }} />
-  </Tabs>
-);
+const tabBar = components.tabBar;
+
+const TabLayout = () => {
+  const inset = useSafeAreaInsets();
+
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} className="tabs-glyph" resizeMode="contain" />
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: Math.max(inset.bottom, tabBar.horizontalInset),
+          height: tabBar.height,
+          borderRadius: tabBar.radius,
+          marginHorizontal: tabBar.horizontalInset,
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarItemStyle: {
+          paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6,
+        },
+        tabBarIconStyle: {
+          width: tabBar.iconFrame,
+          height: tabBar.iconFrame,
+          alignItems: "center",
+        },
+      }}
+    >
+      {tabs.map((item) => (
+        <Tabs.Screen
+          name={item.name}
+          key={item.name}
+          options={{
+            title: item.name,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={item.icon} />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
 
 export default TabLayout;
